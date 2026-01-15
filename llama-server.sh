053354650e07d717ce2c6c87a3dfee319b95e5ff
Juan Vázquez Moreno <vmjuan90@gmail.com>
2026-01-15 11:48:19 +0100

    Add promts

--- llama-server.sh
+++ llama-server.sh
@@ -3,10 +3,13 @@ podman run --rm -it \
   --hooks-dir=/usr/share/containers/oci/hooks.d \
   -p 8080:8080 \
   -v /media/shared/llama-models:/models:ro,Z \
+  -v ./promts:/promts:ro,Z \
   llama-server:latest \
   -m /models/qwen2.5-coder-7b-instruct-q4_k_m.gguf \
   -c 16384 \
   -ngl 30 \
   -b 64 \
   --host 0.0.0.0 \
-  --port 8080
+  --chat-template-file /promts/qwen_soft_tools.jinja \
+  --port 8080 \
+   2>&1 | tee llama_server.log
